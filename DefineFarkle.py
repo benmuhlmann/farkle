6436fa861d5e7592caf142b855d72d3ec3d650b3
benmuhlmann <36239418+benmuhlmann@users.noreply.github.com>
2020-11-27 17:36:27 -0700

    Combine scoring and farkle check into init function

--- DefineFarkle.py
+++ DefineFarkle.py
@@ -12,10 +12,14 @@ class Roll:
     def __init__(self, dice):
         self.dice = dice
         self.score = 0
+        self.farkled = False
         self.face_dict = defaultdict(int)
         self.reverse_dict = defaultdict(list)
         for face in range(1, 7):
             self.face_dict[face]
+        self.roll_dice()
+        self.score_roll()
+        self.farkle_check
 
     def roll_dice(self):
         for i in range(self.dice):
@@ -41,11 +45,11 @@ class Roll:
             return(True)
         else:
             return(False)
-    
+
     # will create an empty list of triplets if none appeared (defaultdict)
     def num_triplets(self):
         return(len(self.reverse_dict[3]))
-        
+
     def num_pairs(self):
         return(len(self.reverse_dict[2]))
 
@@ -54,7 +58,7 @@ class Roll:
             return(True)
         else:
             return(False)
-            
+
     def score_roll(self):
         if self.one_through_six():
             self.score = 1500
@@ -83,8 +87,13 @@ class Roll:
         if self.face_dict[5] < 3:
             self.score += 50*self.face_dict[5]
         return
-     
-     
+
+    def farkle_check(self):
+        if self.score == 0:
+            self.farkled = True
+        return self.farkled
+
+
 class Turn:
     def __init__(self):
         self.dice = 6
