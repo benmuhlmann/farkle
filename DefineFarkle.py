6e7ef7ddf9f0f966a640064298fe26a3406bd384
benmuhlmann <36239418+benmuhlmann@users.noreply.github.com>
2020-11-29 23:21:43 -0700

    Add attribute to track scoring dice

--- DefineFarkle.py
+++ DefineFarkle.py
@@ -12,14 +12,16 @@ class Roll:
     def __init__(self, dice):
         self.dice = dice
         self.score = 0
+        self.scoring_dice = 0
         self.farkled = False
         self.face_dict = defaultdict(int)
         self.reverse_dict = defaultdict(list)
         for face in range(1, 7):
             self.face_dict[face]
+
         self.roll_dice()
         self.score_roll()
-        self.farkle_check
+        self.farkle_check()
 
     def roll_dice(self):
         for i in range(self.dice):
@@ -62,30 +64,40 @@ class Roll:
     def score_roll(self):
         if self.one_through_six():
             self.score = 1500
+            self.scoring_dice = 6
             return
         if self.six_of_a_kind():
             self.score = 3000
+            self.six_of_a_kind = 6
             return
         if self.five_of_a_kind():
             self.score = 2000
+            self.six_of_a_kind = 5
         if self.four_of_a_kind():
             if self.num_pairs == 1:
                 self.score = 1500
+                self.scoring_dice = 6
                 return
             else:
                 self.score = 1000
+                self.scoring_dice = 4
         if self.num_triplets() == 2:
             self.score = 2500
+            self.scoring_dice = 6
             return
         if self.num_triplets() == 1:
             self.score = 100*self.reverse_dict[3][0]
+            self.scoring_dice = 3
         if self.num_pairs == 3:
             self.score = 1500
+            self.scoring_dice = 6
             return
         if self.face_dict[1] < 3:
             self.score += 100*self.face_dict[1]
+            self.scoring_dice += self.face_dict[1]
         if self.face_dict[5] < 3:
             self.score += 50*self.face_dict[5]
+            self.scoring_dice += self.face_dict[5]
         return
 
     def farkle_check(self):
