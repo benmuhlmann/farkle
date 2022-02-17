3a849ce92177ca8718af7a8b429453fadc255f1e
benmuhlmann <36239418+benmuhlmann@users.noreply.github.com>
2022-02-16 22:22:43 -0700

    Simple game class & Script

---
+++ Game.py
@@ -0,0 +1,50 @@
+from DefineFarkle import *
+import time
+
+class Game:
+    """
+    Runs through a game
+    1. get player list
+    2. loop through player list until one player gets 10k
+    """
+    def __init__(self, num_players=2, target_score=10000):
+        self.num_players = num_players
+        self.target_score = target_score
+        self.players = list()
+        self.scores = dict()
+        self.rounds = 0
+
+
+    def get_player_list(self):
+        player_list = list()
+        for i in range(1, self.num_players+1):
+            name = input(f"What's player {i}'s name?")
+            player_list.append(name)
+            self.scores[name] = 0
+            self.players.append(name)
+
+    def game_loop(self):
+        # keep playing until someone hits max score
+        while max(self.scores.values()) < self.target_score:
+            # loop through player list
+            # print out player list
+            # call the turn class
+            # add turn's score to player's score total in self.scores
+            for player in self.players:
+                print(f"\nIt's {player}'s turn")
+                print(f"scores: \n {self.scores}\n")
+                time.sleep(1)
+                print("Rolling...\n")
+                time.sleep(2)
+                current_turn = Turn()
+                self.scores[player] += current_turn.score
+
+if __name__ == "__main__":
+    my_game = Game(2, 3000)
+    my_game.get_player_list()
+    my_game.game_loop()
+
+
+
+
+
