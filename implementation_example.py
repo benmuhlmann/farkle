d19bde42557e8b53513d36b4542878d496722007
benmuhlmann <36239418+benmuhlmann@users.noreply.github.com>
2022-02-27 14:06:15 -0700

    Update call to Roll class in implementation example

--- implementation_example.py
+++ implementation_example.py
@@ -7,14 +7,17 @@ and output the
 percentage of farkles
 """
 import random
-import DefineFarkle
+from DefineFarkle import *
 
 random.seed()
 num_rolls = 10000
 roll_list = []
 
 for roll in range(num_rolls):
-    my_turn = Roll(1)
+    my_turn = Roll(6)
+    my_turn.roll_dice()
+    my_turn.score_roll()
+    my_turn.farkle_check()
     roll_list.append(my_turn.farkled)
 
 # percent of farkles
